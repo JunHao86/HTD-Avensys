842e883cdb42b1ac88f880570fbcf9e002976faa
Emmanuel Lim <limjunhao5714@gmail.com>
2023-06-08 15:18:07 +0800

    Update PerformApp

--- Frontend - React/performapp/src/launch.js
+++ Frontend - React/performapp/src/launch.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState,useMemo } from "react"
 
 export function Launch(){
     //Proof of re-render/re-execute via change of state in Component
@@ -7,13 +7,30 @@ export function Launch(){
 
     const [number,setNumber] = useState(0)
 
+    const [enteredName, setEnteredName] = useState("")
+
+    // enteredName = "Raju"
+
     function cube(num)
     {
-        //Logic to find cube of number
-        return num * num * num
+        let i = 0
+        let anotherNum
+        //Logic to exhibit performance issue (comment either)
+        while (i<100000000)
+        {
+            anotherNum = num * num * num
+            i++
+        }
+        //Standard Logic to find cube of number (comment either)
+        // anotherNum = num * num * num
+        
+        return anotherNum
     }
 
-    let zxc = cube(number)
+    //Cube function called when data in number variable changes
+    let result = useMemo(function(){
+        return cube(number)
+    },[number])
 
     function increaseIt(){
         setNumber(number+1)
@@ -23,11 +40,20 @@ export function Launch(){
         setNumber(number-1)
     }
 
+    function collectIt(event){
+        setEnteredName(event.target.value)
+    }
+
+
+
     return(
         <div>
-            <h2> Number is {number}, Cube is {zxc} </h2>
+            <h2> Number is {number}, Cube is {result} </h2>
             <button onClick={increaseIt}>Increase</button>
             <button onClick={decreaseIt}>Decrease</button>
+
+            <h2> Name is {enteredName}</h2>
+            Enter name: <input type="text" onChange={collectIt}></input>
         </div>
     )
 }
